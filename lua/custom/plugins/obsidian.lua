d262a8aa4bd8142f4ff1c9d2d3a359a6a93601b6
James <jjohnes@allpointretail.com>
2025-06-13 12:14:05 -0400

    added obsidian plugin
    will need to make the workspaces configurable locally

---
+++ lua/custom/plugins/obsidian.lua
@@ -0,0 +1,23 @@
+return {
+  "epwalsh/obsidian.nvim",
+  lazy = true,
+  ft = "markdown",
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "ap-dev",
+        path = "/mnt/c/Users/james/Onedrive/Documents/allpoint-dev"
+      }
+    },
+  },
+}
