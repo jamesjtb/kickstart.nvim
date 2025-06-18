add5c8f5fbd437d9a62011d234114e16c7474805
James <jjohnes@allpointretail.com>
2025-06-18 14:57:13 -0400

    tidy up harpoon config

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,39 +1,96 @@
 local harpoon
 -- basic telescope configuration
-local conf = require("telescope.config").values
+local conf = require('telescope.config').values
 
 local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
 
-    require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-    }):find()
+  require('telescope.pickers')
+    .new({}, {
+      prompt_title = 'Harpoon',
+      finder = require('telescope.finders').new_table {
+        results = file_paths,
+      },
+      previewer = conf.file_previewer {},
+      sorter = conf.generic_sorter {},
+    })
+    :find()
 end
 
 return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = {"nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim"},
-    config = function ()
-        harpoon = require("harpoon")
-        harpoon:setup()
-    end,
-    keys = {
-        { "<leader>af", function() require("harpoon"):list():append() end, desc = "harpoon file", },
-        { "<leader>aa", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu", },
-        { "<leader>a1", function() require("harpoon"):list():select(1) end, desc = "harpoon to file 1", },
-        { "<leader>a2", function() require("harpoon"):list():select(2) end, desc = "harpoon to file 2", },
-        { "<leader>a3", function() require("harpoon"):list():select(3) end, desc = "harpoon to file 3", },
-        { "<leader>a4", function() require("harpoon"):list():select(4) end, desc = "harpoon to file 4", },
-        { "<leader>a5", function() require("harpoon"):list():select(5) end, desc = "harpoon to file 5", },
-        { "<leader>ao", function() toggle_telescope(harpoon:list()) end, desc = "h[a]rpoon window: [o]pen"}
-    }
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+  config = function()
+    harpoon = require 'harpoon'
+    harpoon:setup()
+  end,
+  keys = {
+    {
+      '<leader>hf',
+      function()
+        harpoon:list():add()
+      end,
+      desc = 'harpoon file',
+    },
+    {
+      '<leader>ha',
+      function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = 'harpoon quick menu',
+    },
+    {
+      '<leader>h1',
+      function()
+        harpoon:list():select(1)
+      end,
+      desc = 'select harpoon 1',
+    },
+    {
+      '<leader>h2',
+      function()
+        harpoon:list():select(2)
+      end,
+      desc = 'select harpoon 2',
+    },
+    {
+      '<leader>h3',
+      function()
+        harpoon:list():select(3)
+      end,
+      desc = 'select harpoon 3',
+    },
+    {
+      '<leader>h4',
+      function()
+        harpoon:list():select(4)
+      end,
+      desc = 'select harpoon 4',
+    },
+    {
+      '<leader>hp',
+      function()
+        harpoon:list():prev()
+      end,
+      desc = 'previous harpoon',
+    },
+    {
+      '<leader>hn',
+      function()
+        harpoon:list():next()
+      end,
+      desc = 'next harpoon',
+    },
+    {
+      '<leader>ho',
+      function()
+        toggle_telescope(harpoon:list())
+      end,
+      desc = 'open harpoon',
+    },
+  },
 }
