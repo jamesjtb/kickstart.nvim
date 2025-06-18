94cbc591e6501525d0dd3c7d90a1a91700d950ed
greybard <supagreybard@gmail.com>
2025-06-18 18:18:43 -0400

    remove obsidian workspace

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,24 +1,18 @@
 return {
   'epwalsh/obsidian.nvim',
-  version = 'v3.9.0',
   lazy = true,
   ft = 'markdown',
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'hrsh7th/nvim-cmp',
-    'nvim-telescope/telescope.nvim',
-    'nvim-treesitter',
   },
   opts = {
-    workspaces = {
-      {
-        name = 'allpoint',
-        path = '/mnt/c/Users/james/Onedrive/Documents/allpoint-dev',
-      },
-      {
-        name = 'gbc',
-        path = "/mnt/c/Users/james/obsidian vaults/Greybard's Corner",
-      },
-    },
+    workspaces = {},
   },
 }
