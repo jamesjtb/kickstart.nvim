3fa6de710c83a0da07fea2bf497bd61f8f4b2ffc
James <jjohnes@allpointretail.com>
2025-06-20 16:07:31 -0400

    add keybinds for TSTools user commands

--- lua/custom/plugins/typescript-tools.lua
+++ lua/custom/plugins/typescript-tools.lua
@@ -1,3 +1,9 @@
+vim.keymap.set('n', '<leader>lo', '<cmd>TSToolsOrganizeImports<cr>', { desc = '[L]SP - [O]rganize imports' })
+vim.keymap.set('n', '<leader>lf', '<cmd>TSToolsFixAll<cr>', { desc = '[L]SP - [F]ix fixable errors' })
+vim.keymap.set('n', '<leader>ld', '<cmd>TSToolsGoToSourceDefinition<cr>', { desc = '[L]SP - Go to source [d]efinition' })
+vim.keymap.set('n', '<leader>ln', '<cmd>TSToolsRenameFile<cr>', { desc = '[L]SP - Re[n]ame file' })
+vim.keymap.set('n', '<leader>lr', '<cmd>TSToolsFileReferences<cr>', { desc = '[L]SP - Find file [r]eferences' })
+
 return {
   'pmizio/typescript-tools.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
